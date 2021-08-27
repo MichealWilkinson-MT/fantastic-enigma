d83bb4d1b179db630a872f779c523acf2386939b
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-08-27 14:17:57 +0100

    Working sqs trigger lambda to generate hash and store to dynamodb database.

--- services/dynamodb-writer/go.mod
+++ services/dynamodb-writer/go.mod
@@ -2,4 +2,9 @@ module github.com/MichealWilkinson-MT/fantastic-enigma/services/dynamodb-writer
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.26.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.26.0 // indirect
+	github.com/aws/aws-sdk-go v1.40.31 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
--- services/dynamodb-writer/main.go
+++ services/dynamodb-writer/main.go
@@ -3,24 +3,52 @@ package main
 import (
 	"context"
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("eu-west-2"))
+
 // receives from queue
-func handleRequest(ctx context.Context, sqsEvent events.SQSEvent) (string, error) {
-	
+func handleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
 		messageString := message.Body
+		fmt.Printf("INFO: Hashing %s\n", messageString)
 		hash := sha1.New()
 		hash.Write([]byte(messageString))
 		byteSlice := hash.Sum(nil)
-		return fmt.Sprintf("%x\n", byteSlice), nil
+		hashString := hex.EncodeToString(byteSlice)
+		fmt.Printf("INFO: Got hash %s\n", hashString)
+		putHashInDatabase(messageString, hashString)
+	}
+	return nil
+}
+
+func putHashInDatabase(messageString, hashedResult string) error {
+	fmt.Print("INFO: Writing to DB")
+	input := &dynamodb.PutItemInput {
+		TableName: aws.String("fantastic-enigma-shas"),
+		Item: map[string]*dynamodb.AttributeValue {
+			"InputString" : {
+				S: aws.String(messageString),
+			},
+			"Sha" : {
+				S: aws.String(hashedResult),
+			},
+		},
 	}
 
-	return "", nil
+	_, err := db.PutItem(input)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n" , err)
+	}
+	return err
 }
 
 func main() {
