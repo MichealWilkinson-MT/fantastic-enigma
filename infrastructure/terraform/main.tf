1a717460453c5762161a643f453ec90c8f7ae459
Ian King <ian.antking@gmail.com>
2021-08-31 16:47:32 +0100

    add api gateway and sqs integration

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -88,6 +88,33 @@ data "aws_iam_policy_document" "sqs_trigger_lambda" {
   }
 }
 
+data "aws_iam_policy_document" "api_gateway_logs" {
+    statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:FilterLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "api_gateway_publish_sqs_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage"
+    ]
+    resources = [ aws_sqs_queue.import-queue.arn ]
+  }
+}
+
 data "aws_iam_policy_document" "dynamodbwriter_assume_role_policy" {
   statement {
     effect = "Allow"
@@ -114,9 +141,41 @@ data "aws_iam_policy_document" "importsqs_assume_role_policy" {
   }
 }
 
+data "aws_iam_policy_document" "api_gateway_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        "apigateway.amazonaws.com"
+      ]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_policy" "api_gateway_logs_policy" {
+  name = "apiGatewayLogsPolicy"
+  policy = data.aws_iam_policy_document.api_gateway_logs.json
+}
+
+resource "aws_iam_policy" "api_gateway_publish_sqs_policy" {
+  name = "apiGatewayPublishSQSPolicy"
+  policy = data.aws_iam_policy_document.api_gateway_publish_sqs_policy.json
+}
+
 resource "aws_iam_policy" "hash_function_policy" {
   name   = "MedichecksDatabaseWriteLambdaPolicy"
   policy = data.aws_iam_policy_document.hash_function_policy_doc.json
+}
+
+resource "aws_iam_role" "api_gateway_role" {
+  name = "MedichecksApiGatewayRole"
+  managed_policy_arns = [
+    aws_iam_policy.api_gateway_publish_sqs_policy.arn,
+    aws_iam_policy.api_gateway_logs_policy.arn
+  ]
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_assume_role_policy.json
 }
 
 resource "aws_iam_role" "hash_function_role" {
@@ -132,6 +191,35 @@ resource "aws_sqs_queue" "import-queue" {
   fifo_queue                  = true
   content_based_deduplication = true
   policy = data.aws_iam_policy_document.sqs_trigger_lambda.json
+}
+
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name = "FantasticEnigmaApiv2"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "api_gateway_post_hash_integration" {
+  api_id = aws_apigatewayv2_api.api_gateway.id
+  integration_type = "AWS_PROXY"
+  integration_subtype = "SQS-SendMessage"
+  request_parameters = { 
+    "QueueUrl" : aws_sqs_queue.import-queue.url, 
+    "MessageBody" : "$request.body.string",
+    "MessageGroupId" : "MyGroupId"
+  }
+  credentials_arn = aws_iam_role.api_gateway_role.arn
+}
+
+resource "aws_apigatewayv2_route" "api_gateway_post_hash_route" {
+  api_id = aws_apigatewayv2_api.api_gateway.id
+  route_key = "POST /hash"
+  target = "integrations/${aws_apigatewayv2_integration.api_gateway_post_hash_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "api_gateway_dev_stage" {
+  api_id = aws_apigatewayv2_api.api_gateway.id
+  name = "dev"
+  auto_deploy = true
 }
 
 resource "aws_ssm_parameter" "import-queue-arn" {
