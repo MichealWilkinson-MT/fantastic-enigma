a18f6c3eeed0363ad3a448cd68b3242a0b433eb7
Ian King <ian.antking@gmail.com>
2021-09-03 18:09:18 +0100

    figrued out what we were doing wrong with mocks, also refactored main.go into struct

--- services/dynamodb-writer/main_test.go
+++ services/dynamodb-writer/main_test.go
@@ -8,43 +8,49 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func TestHashInput(test *testing.T) {
-
-	test.Run("Returns a hashed string", func (test *testing.T) {
-		
-		expected := "2582c03f8bbd94611a6d24df12b9ab218a2a3db9"
-		actual := hashInput("Rishi is cool")
-
-		assert.Equal(test, expected, actual)	
-		
-	})
-
-}
-
 type MockDyamodb struct {
 	mock.Mock
 }
 
 func (db *MockDyamodb) PutItem(*dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+	db.Called()
 	return &dynamodb.PutItemOutput{}, nil
+}
+
+func TestHashInput(test *testing.T) {
+
+	test.Run("Returns a hashed string", func (test *testing.T) {
+		handler := Handler{}
+		
+		expected := "2582c03f8bbd94611a6d24df12b9ab218a2a3db9"
+		actual := handler.hashInput("Rishi is cool")
+
+		assert.Equal(test, expected, actual)	
+		
+	})
 }
 
 func TestPutHashDatabase(test *testing.T) {
 
 	test.Run("Calls PutItem", func(test *testing.T) {
 		db := &MockDyamodb{}
-		initDatabase(db)
 
-		putHashInDatabase("Rishi is cool", "2582c03f8bbd94611a6d24df12b9ab218a2a3db9")
+		handler := NewHandler(db, "test-db")
+
+		db.On("PutItem", mock.Anything).Return(nil, nil)
+
+		handler.putHashInDatabase("Rishi is cool", "2582c03f8bbd94611a6d24df12b9ab218a2a3db9")
 	
 		db.AssertCalled(test, "PutItem")
 	})
 
 	test.Run("Given a valid string dynamo-db does not throw an error", func(test *testing.T) {
 		db := &MockDyamodb{}
-		initDatabase(db)
+		handler := NewHandler(db, "test-db")
 
-		err := putHashInDatabase("Rishi is cool", "2582c03f8bbd94611a6d24df12b9ab218a2a3db9")
+		db.On("PutItem", mock.Anything).Return(nil, nil)
+
+		err := handler.putHashInDatabase("Rishi is cool", "2582c03f8bbd94611a6d24df12b9ab218a2a3db9")
 
 		assert.Equal(test, nil, err)
 	})
