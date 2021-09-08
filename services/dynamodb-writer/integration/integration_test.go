2a3231114200108da994e19b1c31cd30662545d7
Ian King <ian.antking@gmail.com>
2021-09-08 12:12:48 +0100

    refactor test into test suite

--- services/dynamodb-writer/integration/integration_test.go
+++ services/dynamodb-writer/integration/integration_test.go
@@ -6,11 +6,13 @@
 package integration
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
@@ -19,11 +21,20 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 var stage string
 
-var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("eu-west-2"))
+//Integration test suite
+type integrationTestSuite struct {
+	suite.Suite
+	db *dynamodb.DynamoDB
+}
+
+func (suite *integrationTestSuite) SetupSuite() {
+	suite.db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("eu-west-2")) //TODO Factor out the explicit DB region
+}
 
 type getItemResponse struct {
 	InputString string `json:"InputString"`
@@ -34,57 +45,61 @@ func init() {
 	flag.StringVar(&stage, "stage", "dev", "Specify the stage to test")
 }
 
-func TestInput(test *testing.T) {
-	test.Run("Trigging lambda puts hash in db", func(test *testing.T) {
-		sess := session.Must(session.NewSessionWithOptions(session.Options{
-			SharedConfigState: session.SharedConfigEnable,
-		}))
+func (suite *integrationTestSuite) TestInput() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-		client := lambda.New(sess, &aws.Config{Region: aws.String("eu-west-2")})
+	client := lambda.New(sess, &aws.Config{Region: aws.String("eu-west-2")})
 
-		request := events.SQSEvent{
-			Records: []events.SQSMessage{
-				{
-					Body: "Alex is the best!",
-				},
+	randomTestString := base64.StdEncoding.EncodeToString([]byte(time.Now().String()))
+
+	request := events.SQSEvent{
+		Records: []events.SQSMessage{
+			{
+				Body: randomTestString,
 			},
-		}
+		},
+	}
 
-		payload, _ := json.Marshal(request)
+	payload, _ := json.Marshal(request)
 
-		functionName := fmt.Sprintf("%s-%s-%s", "fantastic-enigma", stage, "dynamodb-writer")
+	functionName := fmt.Sprintf("%s-%s-%s", "fantastic-enigma", stage, "dynamodb-writer")
 
-		_, err := client.Invoke(&lambda.InvokeInput{
-			FunctionName: aws.String(functionName),
-			Payload:      payload,
-		})
-
-		if err != nil {
-			fmt.Println("Its all gone wrong")
-			os.Exit(1)
-		}
-
-		// check if result is in the database
-		input := &dynamodb.GetItemInput{
-			TableName: aws.String(fmt.Sprintf("fantastic-enigma-shas-%s", stage)),
-			Key: map[string]*dynamodb.AttributeValue{
-				"InputString": {
-					S: aws.String("Alex is the best!"),
-				},
-			},
-		}
-
-		itemOutput, err := db.GetItem(input)
-		if err != nil {
-			fmt.Printf("ERROR: %s\n", err)
-		}
-
-		var resp getItemResponse
-		err = dynamodbattribute.UnmarshalMap(itemOutput.Item, &resp)
-		if err != nil {
-			fmt.Println("Error unmarshalling MyGetItemsFunction response")
-			os.Exit(0)
-		}
-		assert.Equal(test, "Alex is the best!", resp.InputString)
+	_, err := client.Invoke(&lambda.InvokeInput{
+		FunctionName: aws.String(functionName),
+		Payload:      payload,
 	})
+
+	if err != nil {
+		fmt.Println("Its all gone wrong")
+		os.Exit(1)
+	}
+
+	// check if result is in the database
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String(fmt.Sprintf("fantastic-enigma-shas-%s", stage)),
+		Key: map[string]*dynamodb.AttributeValue{
+			"InputString": {
+				S: aws.String(randomTestString),
+			},
+		},
+	}
+
+	itemOutput, err := suite.db.GetItem(input)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+	}
+
+	var resp getItemResponse
+	err = dynamodbattribute.UnmarshalMap(itemOutput.Item, &resp)
+	if err != nil {
+		fmt.Println("Error unmarshalling MyGetItemsFunction response")
+		os.Exit(0)
+	}
+	assert.Equal(suite.T(), randomTestString, resp.InputString)
+}
+
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(integrationTestSuite))
 }
